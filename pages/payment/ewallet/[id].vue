ae6ee02ba82b65b0e79679b901137a19d03fa062
Teguhriyadi <ilham.teguh55@gmail.com>
2024-04-24 15:16:57 +0700

    Modified: Update

--- pages/payment/ewallet/[id].vue
+++ pages/payment/ewallet/[id].vue
@@ -9,11 +9,11 @@
     <template v-if="isLoading">
       <v-skeleton-loader type="paragraph"></v-skeleton-loader>
     </template>
-    <CardBank v-else Subtitle="lorem ipsum dolor sit amet" :Title="data.channel_name" :key="data.id_ewallet"
+    <CardBank v-else :Subtitle="data.channel_name" :Title="data.channel_name" :key="data.id_ewallet"
       class="mb-3" @click="handleRoute(data.channel_code, encryptAmount)">
       <template #avatarImage>
         <v-avatar size="90" class="ms-4" rounded="0">
-          <v-img :src="logoShopee"></v-img>
+          <v-img :src="getEwalletLogo(data.channel_name)"></v-img>
         </v-avatar>
       </template>
     </CardBank>
@@ -22,8 +22,13 @@
 
 <script setup>
 import axios from "axios";
+import LOGO_OVO from "@/assets/images/logo_ewallet/E-WALLET_OVO.png";
+import LOGO_LINK_AJA from "@/assets/images/logo_ewallet/E-WALLET_Link Aja.png";
+import LOGO_ASTRAPAY from "@/assets/images/logo_ewallet/E-WALLET_AstraPay.png";
+
 import Shopee from '@/assets/images/bank_logos/shopee.png'
 import CryptoJS from "crypto-js"
+
 const bank = ref("")
 const loading = ref(false)
 const logoShopee = Shopee
@@ -31,6 +36,17 @@ const encryptAmount = ref("20000")
 onMounted(() => {
   getBank()
 })
+
+const imageLogos = {
+  OVO: LOGO_OVO,
+  LINK_AJA: LOGO_LINK_AJA,
+  ASTRAPAY: LOGO_ASTRAPAY
+}
+
+const getEwalletLogo = (code) => {
+  return imageLogos[code]
+}
+
 const router = useRouter()
 const getBank = async () => {
   try {
